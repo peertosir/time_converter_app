47f2cd88e8541d36f2b088af18d8318d4a41d3da
Ilya Danilov <peertosir@gmail.com>
2020-06-07 22:42:29 +0300

    Initial Commit

---
+++ Converter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Converter
+//
+//  Created by Ilya Danilov on 07.06.2020.
+//  Copyright © 2020 Ilya Danilov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
