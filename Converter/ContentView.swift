d50946b5602e55d09e80a125898cfe9fb970147c
Ilya Danilov <peertosir@gmail.com>
2020-06-08 00:24:46 +0300

    Done without UI prettifying

--- Converter/ContentView.swift
+++ Converter/ContentView.swift
@@ -9,8 +9,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var unitsFrom = 0
+    @State private var inputFrom = ""
+    @State private var unitsFor = 1
+    let units = ["seconds": 1.0, "minutes": 60.0, "hours": 3600.0, "days": 86400.0]
+    var unitsKeyArray: [String] {
+        return Array(units.keys).sorted()
+    }
+    
+    var calculations: (userVal: Double, result: Double) {
+        let userInput = Double(inputFrom) ?? 0
+        let multiplier: Double! = units[unitsKeyArray[unitsFrom]]
+        let divider: Double! = units[unitsKeyArray[unitsFor]]
+        let secFromInput = userInput != 0.0 ? userInput * multiplier : 0
+        let result = secFromInput / divider
+        return (userVal: userInput, result: result)
+    }
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            Form {
+                Section(header: Text("Units from: ")) {
+
+                    Picker("Pick units of time", selection: $unitsFrom) {
+                        ForEach(0 ..< unitsKeyArray.count) {
+                            Text("\(self.unitsKeyArray[$0])")
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                    TextField("Enter the value", text: $inputFrom)
+                }
+                Section(header: Text("Units for: ")) {
+
+                    Picker("Pick units of time", selection: $unitsFor) {
+                        ForEach(0 ..< units.count) {
+                            Text("\(self.unitsKeyArray[$0])")
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                }
+                Section(header: Text("Result:")) {
+                    Text("\(calculations.userVal, specifier: "%.2f") \(unitsKeyArray[unitsFrom]) is \(calculations.result, specifier: "%.2f") \(unitsKeyArray[unitsFor])")
+                  }
+            }
+        .navigationBarTitle("Time converter")
+        }
     }
 }
 
